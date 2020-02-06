387792b700bea101a345165990004217b8c338cf
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-06 20:48:23 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -16,7 +16,7 @@ def print_first_directors_movie_titles
     column_index = 0
     while column_index < nds[row_index].length do
       if nds[row_index][column_index] == "Stephen Spielberg"
-        print `#{nds[row_index][column_index]}\n`
+        pp `#{nds[row_index][column_index]}\n`
       else
         column_index += 1
       end
