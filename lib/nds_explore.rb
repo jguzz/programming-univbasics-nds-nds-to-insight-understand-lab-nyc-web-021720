f9b8a31150409bcd097259a54d68210242570d6a
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-06 21:02:17 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,18 +8,5 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  nds = directors_database
-  row_index = 0
-  while nds.length > row_index do
-    #title
-    column_index = 0
-    while column_index < nds[row_index].length do
-      if nds[row_index][column_index] == "Stephen Spielberg"
-        print "#{nds[row_index][column_index]}\n"
-      else
-        column_index += 1
-      end
-    end
-    row_index += 1
-  end
+pp nds
 end
